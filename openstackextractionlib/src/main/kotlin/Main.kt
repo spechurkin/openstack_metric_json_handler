fa8ced766aded795ec44be35f3310c640ccbf3f7
hololight17 <pechurkins@gmail.com>
2025-08-27 20:32:11 +0300

    full refactor

--- openstackextractionlib/src/main/kotlin/Main.kt
+++ openstackextractionlib/src/main/kotlin/Main.kt
@@ -7,18 +7,7 @@ val identityUrl = "https://10.0.2.15:5000/v3/"
 val login = "admin"
 val password = "59da528341503a01d9f1df2e5fcb5d76f8c07e64ab791c59937a93"
 
-fun toJson(list: List<Any>, metric: String?) {
-    val gson = GsonBuilder().serializeNulls().setPrettyPrinting().create()
-    File("src/main/resources/metric/$metric.json").parentFile?.mkdirs()
-    File("src/main/resources/metric/$metric.json").writeText(gson.toJson(list))
-}
 
-fun toJson(jsonString: String, metric: String?) {
-    val gson = GsonBuilder().serializeNulls().setPrettyPrinting().create()
-    val json = JsonParser.parseString(jsonString).asJsonObject.getAsJsonArray(metric!!.split("/")[1])
-    File("src/main/resources/metric/$metric.json").parentFile?.mkdirs()
-    File("src/main/resources/metric/$metric.json").writeText(gson.toJson(json))
-}
 
 fun main() {
     val wrapper = OpenStackWrapper(identityUrl, login, password, "default", "admin")
