979e94a172f1d2535d99c40bf9c37e9a73fd1628
hololight17 <pechurkins@gmail.com>
2025-09-04 10:49:42 +0300

    build without ssl

--- extractionlib/src/main/kotlin/services/wrappers/OpenStackWrapper.kt
+++ extractionlib/src/main/kotlin/services/wrappers/OpenStackWrapper.kt
@@ -1,6 +1,7 @@
 package services.wrappers
 
 import org.openstack4j.api.OSClient.OSClientV3
+import org.openstack4j.core.transport.Config
 import org.openstack4j.model.common.Identifier
 import org.openstack4j.openstack.OSFactory
 
@@ -8,20 +9,29 @@ class OpenStackWrapper(
     private val authUrl: String,
     private val username: String,
     private val password: String,
-    domain: String,
-    project: String
+    private val domain: String,
+    private val project: String
 ) {
-    private val os: OSClientV3 = OSFactory.builderV3()
-        .endpoint(authUrl)
-        .credentials(username, password, Identifier.byName(domain))
-        .scopeToProject(
-            Identifier.byName(project),
-            Identifier.byName(domain)
-        )
-        .authenticate()
+    private fun setClient(): OSClientV3 {
+        val os: OSClientV3 = OSFactory.builderV3()
+            .endpoint(authUrl)
+            .credentials(username, password, Identifier.byName(domain))
+            .scopeToProject(
+                Identifier.byName(project),
+                Identifier.byName(domain)
+            )
+            .withConfig(Config.newConfig().withSSLVerificationDisabled())
+            .authenticate()
 
-    fun compute() = ComputeWrapper(os)
+        return os
+    }
+
+    fun client(): OSClientV3 {
+        return setClient()
+    }
+
+    fun compute() = ComputeWrapper(client())
     fun identity() = IdentityWrapper(authUrl, username, password)
-    fun blockStorage() = BlockStorageWrapper(os)
-    fun networking() = NetworkingWrapper(os)
+    fun blockStorage() = BlockStorageWrapper(client())
+    fun networking() = NetworkingWrapper(client())
 }
